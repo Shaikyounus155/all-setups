fcbef0a6256c4ce8b0068ea4440a892fe870e2a7
Shaikyounus155 <125484147+Shaikyounus155@users.noreply.github.com>
2025-03-02 12:36:40 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -14,5 +14,5 @@ mv kops-linux-amd64 /usr/local/bin/kops
 aws s3api create-bucket --bucket younus155.k8s.local --region us-east-1
 aws s3api put-bucket-versioning --bucket younus155.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
 export KOPS_STATE_STORE=s3://younus155.k8s.local
-kops create cluster --name rahams.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.medium
-kops update cluster --name rahams.k8s.local --yes --admin
+kops create cluster --name younus155.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.medium
+kops update cluster --name younus155.k8s.local --yes --admin
